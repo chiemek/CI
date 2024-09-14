cd6f1352d9685ab04e2461d3816fafd2f8af040f
chiemek <ekeson1085@gmail.com>
2024-09-14 22:45:17 +0100

    added some apps

--- webapp/obj/Debug/net8.0/webapp.AssemblyInfo.cs
+++ webapp/obj/Debug/net8.0/webapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de9d87ec7eacf8a096b5647842428b0469d478a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43202043dfc5e45be45b226ba99f4abe46218c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
