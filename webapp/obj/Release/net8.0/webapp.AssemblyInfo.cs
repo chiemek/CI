bba7380a8bc0e006a6b1404d4ae42e762794392f
chiemek <ekeson1085@gmail.com>
2024-09-14 15:47:39 +0100

    update web.config

--- webapp/obj/Release/net8.0/webapp.AssemblyInfo.cs
+++ webapp/obj/Release/net8.0/webapp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16dea09d783dd3671aec8c28e766eac998977358")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp")]
